98e3443c0d701b067d46f2e854697017d9f758d5
inhawinter <inhawinter@gmail.com>
2023-07-22 18:30:14 +0900

    python web client v0.4

--- window_client/python/SmartFactoryClient.py
+++ window_client/python/SmartFactoryClient.py
@@ -2,6 +2,7 @@ import requests
 import tkinter as tk
 import urllib.request
 from bs4 import BeautifulSoup
+import json
 
 
 def update_labels():
@@ -10,25 +11,19 @@ def update_labels():
     response = requests.get(web_server_url)
     data = response.text
 
-    lines = data.split("\n")
-
-    ip_address = ''
-    if len(lines) >= 4:
-        ip_address = lines[2].split(": ")[1]
-
+    response_dict = json.loads(data)
+    ip_address = response_dict.get('ip_address', '')
     lbl_ip.config(text=f"IP address: {ip_address}")
-    lbl_temperature.config(text=lines[3])
+    lbl_temperature.config(text=f"Temperature : {round(response_dict.get('temperature_celsius', ''), 2)}C ({round(response_dict.get('temperature_fahrenheit', ''), 2)}F)")
 
     api = 'https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
     urls = urllib.request.urlopen(api).read()
     soup = BeautifulSoup(urls, "html.parser")
-    #cities = soup.find_all("city")
     tmin = soup.find_all("tmn")  # lowest temperature
     tmax = soup.find_all("tmx")  # highest temperature
     yyyymmdd = soup.find_all("tmef")  # yyyy-mm-dd 00:00
 
     lbl_incheon_temperature.config(text=f'City : Incheon, Min temp. : {tmin[1 * 13].string}C, Max temp. : {tmax[1 * 13].string}C\nDates(Midterm forecast) : {yyyymmdd[1*13].string}')
-    #print(f'{cities[1].string} Min temp. : {tmin[1 * 13].string}, Max temp. : {tmax[1 * 13].string}')
 
 
 # create gui component
